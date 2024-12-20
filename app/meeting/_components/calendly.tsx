75d4d2c0c3d2f8ff1e418b22cfe690d1e56d5b6c
whyashthakker <nynansnh@gmail.com>
2024-12-20 12:23:53 +0530

    calendly fix

--- app/meeting/_components/calendly.tsx
+++ app/meeting/_components/calendly.tsx
@@ -1,40 +1,16 @@
 "use client";
 
-import { useEffect } from 'react';
+import { InlineWidget } from "react-calendly";
 
-const ApolloScheduler = () => {
-  useEffect(() => {
-    // Load Apollo's scheduling widget script
-    const script = document.createElement('script');
-    script.src = 'https://cdn.apollo.io/scheduling-embed.js';
-    script.async = true;
-    document.body.appendChild(script);
 
-    // Initialize the widget once the script loads
-    script.onload = () => {
-      // @ts-ignore - Apollo's scheduling widget is loaded globally
-      if (window.ApolloMeetings) {
-        // @ts-ignore
-        window.ApolloMeetings.initWidget({
-          appId: 'YOUR_APP_ID', // You'll need to get this from Apollo
-          schedulingLink: 'geeta_revns/15-min',
-          // Optional: Specify where to mount the widget
-          // domElement: document.getElementById('apollo-scheduling-embed')
-        });
-      }
-    };
+const Calendly =  () => {
 
-    // Cleanup on unmount
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return (
-    <div className="w-full h-screen">
-      <div id="apollo-scheduling-embed" className="w-full h-full" />
-    </div>
-  );
-};
-
-export default ApolloScheduler;
+    return ( 
+    <div className="w-full ">
+      <InlineWidget
+      styles={{ height: "1000px" }}
+      url="https://calendly.com/explainx/discussion" />
+    </div> );
+}
+ 
+export default Calendly;
